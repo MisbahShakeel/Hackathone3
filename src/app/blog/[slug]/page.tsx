3b4bf637e319f81754320addf4350a17bcf7869c
MisbahShakeel <misbahshakeeel984@gmail.com>
2025-02-07 06:03:13 +0500

    update

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,25 +1,29 @@
 import { client } from "@/sanity/lib/client";
 import { urlFor } from "@/sanity/lib/image";
+import Blog from "@/types/blog";
 import Image from "next/image";
+import { notFound } from "next/navigation"; // Import for handling 404
 
-interface PageProps {
-  params: { slug: string };
+interface BlogPageProps {
+  params: Promise<{ slug: string }>;
 }
 
-const BlogPage = async ({ params }: PageProps) => {
-  const { slug } = params; 
+export default async function BlogPage({ params }: BlogPageProps): Promise<React.ReactElement> {
+  
 
-  const blogQuery = `*[_type == 'blog' && slug.current == '${slug}'] {
+  const { slug } = await params;
+
+  const blogQuery = `*[_type == 'blog' && slug.current == $slug] {
     heading,
     image,
     description,
     blogDate
   }`;
 
-  const sanityData = await client.fetch(blogQuery);
+  const sanityData = await client.fetch(blogQuery, { slug });
 
   if (!sanityData || sanityData.length === 0) {
-    return <p className="text-center text-gray-600">No blog found.</p>;
+    return notFound(); // Show 404 page if no blog is found
   }
 
   return (
@@ -28,14 +32,12 @@ const BlogPage = async ({ params }: PageProps) => {
         {sanityData.map((blog: { heading: string; image: string; description: string; blogDate: string }, index: number) => (
           <div key={index}>
             <h2 className="text-center text-4xl font-lato font-bold mb-6 text-[#151875]">{blog.heading}</h2>
-            <Image src={urlFor(blog.image).url()} alt={blog.heading} width={400} height={400} className="sm:w-[600px] md:w-[700px] lg:w-full" />
+            <Image src={urlFor(blog.image).url()} alt={blog.heading} width={700} height={400} className="w-full" />
             <p className="text-gray-600 text-sm mt-4">{new Date(blog.blogDate).toDateString()}</p>
-            <p className="text-gray-600 w-[350px] sm:w-[550px] md:w-[600px] lg:w-[900px] mt-2 text-center">{blog.description}</p>
+            <p className="text-gray-600 w-full mt-2 text-center">{blog.description}</p>
           </div>
         ))}
       </div>
     </div>
   );
-};
-
-export default BlogPage;
+}
